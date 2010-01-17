240cc924fddd6b0aed4ffa7b65b4f913536e688c
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2010-01-17 02:13:46 +0000

    Saner Layout Computations. Better support for showing factor-type data.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/splots@44036 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotScreen.R
+++ R/plotScreen.R
@@ -6,8 +6,7 @@ plotScreen = function(z,
                        do.legend = TRUE,
                        nx = 24,
                        ny = 16,
-                       fill = c("blue", "white", "red"), 
-                       abris = "#333333",
+                       fill,
                        na.fill = "grey",
                        do.grid.newpage = TRUE) { 
     if (missing(z))
@@ -17,10 +16,6 @@ plotScreen = function(z,
     if (length(z) < 1L)
         stop("'z' must have length >= 1.")
     
-    if(length(fill) < 2L)
-      stop("'fill' must have length >=2 since it is used to compute the color ",
-           "ramp that represent the values in z.")
-
     if (!((length(ncol)==1L)&&(ncol>0L)&&!is.na(ncol)))
       stop("'ncol' must be a positive integer of length 1.")
     if (!((length(nx)  ==1L)&&(  nx>0L)&&!is.na(nx)))
@@ -32,22 +27,50 @@ plotScreen = function(z,
       stop("'do.names' must be logical of length 1.")
     if(!(is.logical(do.legend) && (length(do.legend)==1L) && !is.na(do.legend)))
       stop("'do.legend' must be logical of length 1.")
-    
-    for(i in seq(along=z))
-      z[[i]][!is.finite(z[[i]])] = NA
 
-    if(!missing(zrange)){
-      zrange = sort(zrange)
-      if((length(zrange)!=2) || (!all(is.finite(zrange))) || (zrange[1]>=zrange[2]))
-        stop("'zrange' must be of length 2 with finite values.")
-      z = lapply(z, function(v) { v[v<zrange[1]]=zrange[1]; v[v>zrange[2]]=zrange[2]; return(v) })
-    } else {
-      values = unlist(z)
-      med = median(values, na.rm=TRUE)
-      zrange = med + c(-1, +1) * max(abs(values-med), na.rm=TRUE)
+    ## Figure out the type of the elements of z: should all be the same, factor
+    ## or numeric
+    if(is.numeric(z[[1]])) {
+      ## --- z is numeric ---
+      wh = which(!sapply(z, is.numeric))
+      if(length(wh)>0)
+        stop(sprintf("The elements of 'z' should all have the same type. Element 1 is numeric, but %d is not.\n"), wh[1])
+      
+      for(i in seq(along=z))
+        z[[i]][!is.finite(z[[i]])] = NA
+
+      if(!missing(zrange)){
+        zrange = sort(zrange)
+        if((length(zrange)!=2) || (!all(is.finite(zrange))) || (zrange[1]>=zrange[2]))
+          stop("'zrange' must be of length 2 with finite values.")
+        z = lapply(z, function(v) { v[v<zrange[1]]=zrange[1]; v[v>zrange[2]]=zrange[2]; return(v) })
+      } else {
+        values = unlist(z)
+        med = median(values, na.rm=TRUE)
+        zrange = med + c(-1, +1) * max(abs(values-med), na.rm=TRUE)
+      }
+
+      if(missing(fill))
+        fill = c("blue", "white", "red")
+      colRamp = colorRamp(fill)
+      
+    } else if (is.factor(z[[1]])) {
+      ## --- z is factor ---
+      wh = which(!sapply(z, is.factor))
+      if(length(wh)>0)
+        stop(sprintf("The elements of 'z' should all have the same type. Element 1 is factor, but %d is not.\n"), wh[1])
+      levs = levels(unlist(z))
+      if(!missing(zrange)){
+        if(!all(levs %in% zrange))
+          stop("'z' is a list of factors, but it contains levels that are not contained in 'zrange'.")
+      } else {
+        zrange = levs
+      }
+      
+      if(missing(fill))
+        fill = rep(brewer.pal(9, "Set1"), ceiling(length(zrange)/9))[seq(along=zrange)]
+      
     }
-    
-    colRamp = colorRamp(fill)
 
     if(is.null(names(z)))
       names(z) = paste(seq(along=z))
@@ -68,16 +91,27 @@ plotScreen = function(z,
     }
 
     if(do.legend) {
-      ys = pretty(zrange, 10)
-      ys = ys[-c(1,length(ys))]
+      
+      if(is.numeric(zrange)){
+        yPos  = pretty(zrange, 10)[2:8]
+        yText = paste(yPos)
+        
+        steps = (seq(yPos[1], yPos[length(yPos)], length=50) - zrange[1]) / diff(zrange)
+        mcol = colRamp(steps) / 256
+        col = do.call(rgb, lapply(1:3, function(j) mcol[,j]))
+        
+      } else {
+        yPos  = seq(along=zrange)
+        yText = zrange
+        col = fill
+      }
+
+      dyPos = yPos[2]-yPos[1]
+      
       barWidth  = 8
-      textWidth = max(nchar(paste(ys)))+2
+      textWidth = max(nchar(yText))+2
       unitWidth = unit(barWidth+textWidth, "strwidth", data="x")
       mainViewportWidth  = mainViewportWidth - unitWidth
-      
-      steps = (seq(ys[1], ys[length(ys)], length=50) - zrange[1]) / diff(zrange)
-      mcol = colRamp(steps) / 256
-      col = do.call(rgb, lapply(1:3, function(j) mcol[,j]))
       
       pushViewport(viewport(
           x = unit(1, "npc"),
@@ -86,18 +120,18 @@ plotScreen = function(z,
           height = unit(0.7, "npc"),
           just = c("right", "center"),                      
           xscale = c(0, barWidth+textWidth),                      
-          yscale = ys[c(1, length(ys))] ))
+          yscale = c(yPos[1] - dyPos, yPos[length(yPos)])))
 
       grid.raster(matrix(rev(col), ncol=1, nrow=length(col)),
                   interpolate = FALSE,
-                  x = 2, width = barWidth-2, 
-                  y = zrange[1], height=diff(zrange),
-                  default.units="native", just=c("left", "bottom"))
+                  x = unit(2, "native"), width  = unit(barWidth-2, "native"), 
+                  y = unit(0, "npc"),    height = unit(1, "npc"),
+                  just=c("left", "bottom"))
                  
-      grid.text(label = paste(ys),
+      grid.text(label = yText,
                 x = barWidth,
-                y = ys,
-                default.units="native", just=c("left", "bottom"),
+                y = yPos - dyPos/2,
+                default.units="native", just=c("left", "center"),
                 gp = gpar(cex=1, fill="black"))
       
       popViewport()
@@ -120,13 +154,16 @@ plotScreen = function(z,
       for (j in seq_len(nrow)) {
         index = (j-1)*ncol + i
         if (index <= length(z)) {
-          zval = (z[[index]] - zrange[1]) / diff(zrange)
-          zval = ifelse(zval<0, 0, ifelse(zval>1, 1, zval)) ## robustness against rounding errors
-          mcol = colRamp(zval) / 256
-          mcol[is.na(zval), ] = 0 ## 'rgb' does not like NA
-          col = do.call(rgb, lapply(1:3, function(j) mcol[,j]))
-          col[is.na(zval)] = na.fill
-    
+          if(is.numeric(zrange)){
+            zval = (z[[index]] - zrange[1]) / diff(zrange)
+            zval = ifelse(zval<0, 0, ifelse(zval>1, 1, zval)) ## robustness against rounding errors
+            mcol = colRamp(zval) / 256
+            mcol[is.na(zval), ] = 0 ## 'rgb' does not like NA
+            col = do.call(rgb, lapply(1:3, function(j) mcol[,j]))
+            col[is.na(zval)] = na.fill
+          } else {
+            col = fill[as.integer(z[[index]])]
+          }
           grid.raster(matrix(col, nrow=ny, ncol=nx, byrow=TRUE),
                       x=i, y=nrow-j+1, width=0.95, height=plateHeight,
                       interpolate=FALSE, default.units="native",
